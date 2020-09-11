ac12d2fb61b792c057c6daa86bf456dfeb1e36f5
Davi Hillesheim <davi-241@hotmail.com>
2020-09-11 19:10:52 -0300

    remove unnecessary comments

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './SortingVisualizer.css';
 import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
 
-// Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
 
-// Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 310;
 
-// This is the main color of the array bars.
 const PRIMARY_COLOR = 'turquoise';
 
-// This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
 
 export default class SortingVisualizer extends React.Component {
